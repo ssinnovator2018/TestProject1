77adc3fa84723ec96c7604ff9202cb472ab8401c
shital-shubham <shital-shubham@Shubham>
2021-09-13 11:20:39 +0530

    Automation switching practice

--- Automation_practice/src/Automation/Switching_tabs.java
+++ Automation_practice/src/Automation/Switching_tabs.java
@@ -1,27 +1,22 @@
+package Automation;
+
 import java.util.ArrayList;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Firstday {
+public class Switching_tabs {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Selenium\\Selenium server files latest\\Latest Chromedriver\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://www.amazon.com");
 		((JavascriptExecutor) driver).executeScript("window.open('http://cricbuzz.com/')");
 		 ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-		 String handleName = tabs.get(0);
+		 String handleName = tabs.get(1);
 		 driver.switchTo().window(handleName);
 		 driver.navigate().to("http://www.instagram.com/");
-		 handleName = tabs.get(0);
-		 driver.switchTo().window(handleName);
-		 driver.navigate().to("http://www.cricbuzz.com/");
-		 ((JavascriptExecutor) driver).executeScript("window.open('https://www.amazon.com/')");
-		 driver.switchTo().window(tabs.get(0));
-		 driver.get("https://www.zerodha.com/");
-		 
-		 
 	}
-} 
+
+}
